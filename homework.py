97533a528ea6d22da199dc7d3d024e9b67ac13cb
Rinat Khaibullin <rinatkhaibullin@MacBook-Pro-Rinat.local>
2023-01-09 01:29:51 +0900

    Программа для фитнесбраслета

--- homework.py
+++ homework.py
@@ -11,11 +11,11 @@ class InfoMessage:
 
     def __init__(self, training_type: str, duration:
                  float, distance: float, speed: float, calories: float):
-            self.training_type = training_type
-            self.duration = duration
-            self.distance = distance
-            self.speed = speed
-            self.calories = calories
+        self.training_type = training_type
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
+        self.calories = calories
 
     def get_message(self) -> str:
         return (f'Тип тренировки: {self.training_type};'
